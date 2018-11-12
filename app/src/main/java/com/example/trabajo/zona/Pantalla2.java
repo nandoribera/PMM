6847e76ea65fc53ef4efead87711fe8fb558cd8a
nandoribera <31983050+nandoribera@users.noreply.github.com>
2018-11-12 01:09:00 +0100

    examen
    Examen

---
+++ app/src/main/java/com/example/trabajo/zona/Pantalla2.java
@@ -0,0 +1,58 @@
+package com.example.trabajo.zona;
+import android.*;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.trabajo.zona.Pantalla1;
+
+public class Pantalla2 extends AppCompatActivity {
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2);
+        Intent intent = getIntent();
+        final TextView continente = (TextView) findViewById(R.id.continente2);
+        final TextView zona = (TextView) findViewById(R.id.zona2);
+        final TextView peso = (TextView) findViewById(R.id.peso2);
+        final TextView envio = (TextView) findViewById(R.id.envio2);
+        final TextView precio = (TextView) findViewById(R.id.precio2);
+        final ImageView imagen2 = (ImageView) findViewById(R.id.imagen2);
+        final Button volver = (Button) findViewById(R.id.volver);
+        Zona zonaObjeto = (Zona) intent.getSerializableExtra("Objeto");
+        float precioTotal = zonaObjeto.getPeso() + zonaObjeto.getPrecio();
+        continente.setText(zonaObjeto.getContinente());
+        zona.setText(zonaObjeto.getNombre());
+        peso.setText(String.valueOf(zonaObjeto.getPeso()));
+        envio.setText(zonaObjeto.getEnvio());
+        imagen2.setBackground(getDrawable(zonaObjeto.getImagen()));
+        if(zonaObjeto.getRegalo() == true){
+            precioTotal += 5;
+        }
+        if(zonaObjeto.getDedicatoria() == true){
+            precioTotal += 7;
+        }
+        if(zonaObjeto.getEnvio().equalsIgnoreCase("Tarifa Normal")){
+            precioTotal += 5;
+        }else{
+            precioTotal += 20;
+        }
+        precio.setText(String.valueOf(precioTotal));
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Pantalla2.this, Pantalla1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
